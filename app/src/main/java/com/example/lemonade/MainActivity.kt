efb901057070192d8db3cbb8a0cf54504ed69ddc
abeni-al7 <abenezeralebachew3@gmail.com>
2025-03-09 17:59:42 +0300

    Build the UI and the logic

--- app/src/main/java/com/example/lemonade/MainActivity.kt
+++ app/src/main/java/com/example/lemonade/MainActivity.kt
@@ -4,23 +4,61 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.lemonade.ui.theme.LemonadeTheme
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             LemonadeTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
+                Scaffold(
+                    modifier = Modifier.fillMaxSize(),
+                    topBar = {
+                        CenterAlignedTopAppBar(
+                            colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
+                                containerColor = Color.Blue
+                            ),
+                            title = {
+                                Text(
+                                    stringResource(id = R.string.app_name),
+                                    maxLines = 1,
+                                    overflow = TextOverflow.Ellipsis
+                                )
+                            }
+                        )
+                    }
+                ) { innerPadding ->
+                    LemonadeApp(
                         name = "Android",
                         modifier = Modifier.padding(innerPadding)
                     )
@@ -31,17 +69,71 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun LemonadeApp(name: String, modifier: Modifier = Modifier) {
+    var step by remember { mutableStateOf(1) }
+    var imageResource = when (step) {
+        1 -> R.drawable.lemon_tree
+        2 -> R.drawable.lemon_squeeze
+        3 -> R.drawable.lemon_drink
+        else -> R.drawable.lemon_restart
+    }
+    var textResource = when (step) {
+        1 -> R.string.select_lemon
+        2 -> R.string.squeeze_lemon
+        3 -> R.string.drink_lemon
+        else -> R.string.restart_lemon
+    }
+    var contentDescription = when (step) {
+        1 -> R.string.select_lemon_image
+        2 -> R.string.squeeze_lemon_image
+        3 -> R.string.drink_lemon_image
+        else -> R.string.restart_lemon_image
+    }
+
+    Column(
+        modifier = modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Button(
+            onClick = {
+                when (step) {
+                    2 -> {
+                        var count = (2..4).random()
+                        while (count > 0) {
+                            count--
+                            if (count == 0) {
+                                step = 3
+                            }
+                        }
+                    }
+                    4 -> {
+                        step = 1
+                    }
+                    else -> {
+                        step++
+                    }
+                }
+            },
+            shape = RoundedCornerShape(20.dp)
+        ) {
+            Image(
+                painter = painterResource(imageResource),
+                contentDescription = stringResource(contentDescription)
+            )
+        }
+        Spacer(modifier = Modifier.padding(16.dp))
+        Text(
+            text = stringResource(textResource),
+            fontSize = 18.sp
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     LemonadeTheme {
-        Greeting("Android")
+        LemonadeApp("Android")
     }
 }
